022ba91a71c16d496155ad8145391dd142c7acd7
Alexis Martínez <alemarcha26@gmail.com>
2015-07-01 23:16:49 +0200

    Commit con api funcionando con JSON

---
+++ Trabajo-fin-master-us/src/main/java/api/rest/HelloWorldController.java
@@ -0,0 +1,23 @@
+package api.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import models.User;
+
+@RestController
+public class HelloWorldController {
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public User hello() {
+		User u=new User();
+		u.setEmail("aaaa");
+		u.setFirstName("aafff");
+		u.setLastName("sdf");
+		u.setUserid(1);
+		return u;
+	}
+
+	
+
+}
